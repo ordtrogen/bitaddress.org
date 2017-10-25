094337fb9194cb3c56ea590f63ccdad238c967bf
Ordtrogen Översättning <ordtrogen@users.noreply.github.com>
2017-10-25 07:49:02 +0200

    added file, editing Readme.md

---
+++ src/culture/template.js
@@ -0,0 +1,13 @@
+﻿(function (translator) {
+	translator.translations["LANGUAGE"] = {
+		"singlelabelprivatekey": "Privat nyckel",
+		"singletip1": "<b>En bitcoinplånbok<\/b> är helt enkelt ett par, bestående av en bitcoinadress och dess tillhörande privata nyckel. En sådan plånbok har skapats åt dig i din webbläsare och visas ovan.",
+		"singletip2": "<b>För att skydda denna plånbok<\/b> måste du skriva ut eller på annat spara bitcoinadressen och den privata nyckeln. Det är viktigt att skapa en backup-kopia av den privata nyckeln och spara den på en säker plats. Denna webbplats har ingen kännedom om din privata nyckel. Om du är van att använda PGP kan du ladda ner den kompletta HTML-sidan och verifiera att du har en autentisk version från webbplatsens upphovsmakare genom att matcha SHA256-hashen av sidans HTML med SHA256-hashen som finns tillgänglig i det signerade dokument med versionshistorik som finns länkat i sidfoten på webbplatsen. Om du lämnar\/laddar om webbsidan eller trycker på knappen \"Skapa ny adress\" så kommer en ny privat nyckel att skapas och den tidigare visade privata nyckeln går inte längre att komma åt. Du måste hålla din privata bitcoinnyckel hemlig. Om du delar den privata nyckeln med någon annan så kan denna person komma åt och spendera alla bitcoin som finns på adressen. Om du skriver ut plånboken så bör du förvara den i en plastpåse för att skydda den från väta. Behandla en pappersplånbok som om den var kontanter.",
+		"singletip3": "<b>För över pengar<\/b> till denna plånbok genom att be andra att skicka bitcoin till din bitcoinadress.",
+		"singletip4": "<b>Kolla ditt saldo<\/b> genom att gå till blockchain.info eller blockexplorer.com och ange din bitcoinadress.",
+		"singletip5": "<b>Spendera dina bitcoin<\/b> genom att gå till blockchain.info och svepa din privata nyckels hela saldo till ditt konto på deras webbplats. Alternativt kan du ladda ner något av de populära bitcoinklientprogrammen och importera din privata nyckel till klientens plånbok. Tänk på att när du importerar din nyckel till en bitcoinklient och spenderar pengar så kommer din nyckel att sparas ihop med andra privata nycklar i klientens plånbok. När du genomför en transaktion kommer din växel att skickas till en annan bitcoinadress inom klientens plånbok. Därför måste du ta en backup av klientplånboken och spara den på ett säkert eftersom dina återstående bitcoin kommer att sparas där. Satoshi förespråkade att man aldrig ska radera en plånbok.",
+		"singleshare": "PUBLIK",
+		"singlesecret": "HEMLIG",
+	};
+
+})(ninja.translator);
